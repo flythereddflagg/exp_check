60d4928383ab9de1f94a90e609aed3920f3cd7dd
Mark Redd <redddogjr@gmail.com>
2017-10-23 19:42:11 -0600

    Saving to csv is best

--- ls_man.py
+++ ls_man.py
@@ -6,22 +6,25 @@ The food list manager for the exp_check program.
 """
 from csv import reader, writer
 from operator import itemgetter
+from os.path import exists
 
 class ListManager():
-    def __init__(self):
-        f = open('lst.csv', 'rb')
-        raw_data = reader(f)
-        self.food_list = list(raw_data)
-        f.close()      
-    
+    def __init__(self, filename):
+        self.filename = filename
+        self.food_list = []
+        if not exists(self.filename):
+            self.update_csv()
+        with open(self.filename, 'rb') as f:
+            raw_data = reader(f)
+            self.food_list = list(raw_data)
+
     def update_csv(self):
-        f = open('lst.csv', 'wb')
-        csv_writer = writer(f)
-        csv_writer.writerows(self.food_list)
-        f.close()
+        with open(self.filename, 'wb') as f:
+            csv_writer = writer(f)
+            csv_writer.writerows(self.food_list)
     
     def add_item(self, year, month, day, name):
-        self.food_list.append([year, month, day, name])
+        self.food_list.append([str(year), str(month), str(day), name])
         self.update_csv()
     
     def delete_item(self, name):
@@ -31,40 +34,51 @@ class ListManager():
                 return
         raise ValueError("Item Not Found!")
     
-    def sort_by(self, column):
-        if   column == "date":
+    def sort_by(self, k_str):
+        if   k_str == "date":
             sortkey = lambda x: (int(x[0]),int(x[1]),int(x[2]),x[3])
-        elif column == "name":
+        elif k_str == "name":
             sortkey = itemgetter(3)
-        else: raise ValueError("sortby doesn't have that!")
+        else: raise ValueError("sort_by doesn't have that!")
         
         self.food_list = sorted(self.food_list, key=sortkey)
         self.update_csv()
+    
+    def to_string(self):
+        return '\n'.join([','.join(i) for i in self.food_list])
 
 def test_main():
-    nn = ListManager()
-
-    for i in nn.food_list:
-        print i
+    nn = ListManager("lst.csv")
+    '''
+    ls = [
+        [12,3,1,'greens'],
+        [17,2,3,'beans'],
+        [17,2,3,'chicken'],
+        [17,2,3,'potatoes'],
+        [17,2,3,'rice'],
+        [17,2,3,'watermelon'],
+        [17,11,12,'goo'],
+        [19,5,29,'peas'],
+    ]
+    for i in ls:
+        nn.add_item(*i)
+    '''   
+    print nn.to_string()
     print ""
-    nn.add_item('17', '12', '10', 'food_stuff')
-    #nn.add_item('17', '12', '11', 'food_stuff')
+    nn.add_item('17', '12', '10', 'food stuff')
 
-    for i in nn.food_list:
-        print i
+    print nn.to_string()
     print ""
 
-    nn.delete_item("food_stuff")
+    nn.delete_item("food stuff")
     nn.sort_by("name")
 
-    for i in nn.food_list:
-        print i
+    print nn.to_string()
     print ""
 
     nn.sort_by("date")
 
-    for i in nn.food_list:
-        print i
+    print nn.to_string()
     print ""
 
 if __name__ == '__main__':
