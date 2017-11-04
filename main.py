263718f17d9694801bf46d71da13e54fe571ed95
Mark Redd <redddogjr@gmail.com>
2017-11-03 19:20:17 -0600

    check out the screen thing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 #from kivy.lang import Builder
 from kivy.uix.recycleview import RecycleView
@@ -11,26 +14,9 @@ from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from ls_man import ListManager
 
-"""Builder.load_string('''
-<SelectableLabel>:
-    # Draw a background to indicate selection
-    canvas.before:
-        Color:
-            rgba: (.0, 0.9, .1, .3) if self.selected else (0, 0, 0, 1)
-        Rectangle:
-            pos: self.pos
-            size: self.size
-<RV>:
-    viewclass: 'SelectableLabel'
-    SelectableRecycleBoxLayout:
-        default_size: None, dp(56)
-        default_size_hint: 1, None
-        size_hint_y: None
-        height: self.minimum_height
-        orientation: 'vertical'
-        multiselect: True
-        touch_multiselect: True
-''')"""
+Config.set('graphics', 'width', '250')
+Config.set('graphics', 'height', '400')
+
 
 
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
@@ -65,14 +51,22 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
         else:
             print("selection removed for {0}".format(rv.data[index]))
 
-            
-class Buttons(BoxLayout):
-    def __init__(self):
+
+
+class RV(RecycleView):
+    def __init__(self, data, **kwargs):
+        super(RV, self).__init__(**kwargs)
+        self.data = [{'text': str(x)} for x in data]
+
+        
+class ExChGUI(GridLayout):
+    def __init__(self, lm, rv):
+        self.lm = lm
+        self.rv = rv
         self.init_gui()
-        self.minimum_height=23
     
     def init_gui(self):
-        super(Buttons, self).__init__(orientation='horizontal', minimum_height=100)
+        super(ExChGUI, self).__init__(rows=3)
         
         self.add_button = Button(text="ADD", font_size=14,
             size_hint_y=0.1)
@@ -84,31 +78,15 @@ class Buttons(BoxLayout):
         self.add_button2.bind(on_press=self.callback)
         self.add_widget(self.add_button2)
         
-    def callback(self, instance):
-        print('The button <%s> is being pressed' % instance.text)
-
-
-class RV(RecycleView):
-    def __init__(self, data, **kwargs):
-        super(RV, self).__init__(**kwargs)
-        self.data = [{'text': str(x)} for x in data]
-
-        
-class ExChGUI(BoxLayout):
-    def __init__(self, lm, rv, btn):
-        self.lm = lm
-        self.rv = rv
-        self.btn = btn
-        self.init_gui()
-    
-    def init_gui(self):
-        super(ExChGUI, self).__init__(orientation='vertical')
-        
-        self.add_widget(self.btn)
         self.add_widget(self.rv)
-
-
-
+        
+    def callback(self, instance):
+        if instance.text == "ADD":
+            do_a_thing()
+        elif instance.text == "Delete":
+            print("You pressed delete!")
+        else:
+            print('The button <%s> is being pressed' % instance.text)
 
 
 class ExpCheckApp(App):
@@ -116,8 +94,7 @@ class ExpCheckApp(App):
         lm = ListManager("lst.csv")
         data = lm.to_string().split('\n')
         rv = RV(data)
-        btn = Buttons()
-        gui = ExChGUI(lm, rv,btn)
+        gui = ExChGUI(lm, rv)
         return gui
 
 
