28404cdfc56e2cb05fbf02aa7f5b8b464169f562
Mark <redddogjr@gmail.com>
2019-11-25 17:26:27 -0700

    need linux to compile this thing

--- exp_check/pyqt_version/app.py
+++ exp_check/pyqt_version/app.py
@@ -6,14 +6,17 @@ class FoodGui():
     def __init__(self):
         self.home = HomeScreen(self)
         self.add_screen = AddScreen(self)
-        
+
         self.stack = QStackedWidget()
         self.stack.addWidget(self.home)
         self.stack.addWidget(self.add_screen)
         self.stack.show()
 
 
-if __name__ == '__main__':
+def main():
     app = QApplication([])
     gui = FoodGui()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
