4937b864ee4b80fc73b4bcdcdbdab42f5275a84f
Mark Redd <redddogjr@gmail.com>
2019-11-29 19:48:18 -0700

    added makefiles and am more or less ready to compile but there are compilation problems

--- exp_check_pyqt/download.sh
+++
@@ -1,5 +0,0 @@
-
-wget http://download.qt.io/official_releases/qt/5.12/5.12.2/single/qt-everywhere-src-5.12.2.tar.xz
-wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz
-wget https://www.riverbankcomputing.com/static/Downloads/sip/4.19.15/sip-4.19.15.tar.gz
-wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.12.1/PyQt5_gpl-5.12.1.tar.gz
