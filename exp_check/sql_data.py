e75d9cae845bc49ae1e6a739807b27c8ddaac54b
Mark Redd <redddogjr@gmail.com>
2024-12-04 21:11:53 -0700

    added most if not all the functionality of the original data manager. Now to connect to ui

--- exp_check/sql_data.py
+++ exp_check/sql_data.py
@@ -1,49 +1,93 @@
 import sqlite3 as sql
+import pathlib
+import datetime
+import time
 
-def create(path:str):
-    with open(path, 'w') as f:
-        pass
+main_table = "food_data"
+meta_table = "meta_data"
+
+new_database_script = f"""
+    CREATE TABLE {main_table}(food, date added, expiration date);
+    CREATE TABLE {meta_table}(key, value);
+    INSERT INTO {meta_table} VALUES('last write time', 0);
+    INSERT INTO {meta_table} VALUES('last read time', 0);
+"""
+
+update_write_cmd = (
+    f"UPDATE {meta_table} SET value = ? "
+    "WHERE key='last write time'"
+)
+update_read_cmd = (
+    f"UPDATE {meta_table} SET value = ? "
+    "WHERE key='last read time'"
+)
+insert_cmd = f"INSERT INTO {main_table} VALUES(?, ?, ?)"
+delete_cmd = f"DELETE FROM {main_table} where food=?"
+get_all_cmd = f"SELECT * from {main_table}"
+get_meta_cmd = f"SELECT * from {meta_table}"
+
+date_format = "%Y%m%d%H%M"
+
+def get_now():
+    return datetime.datetime.now().strftime(date_format)
+
+class SQLiteDB():
+    def __init__(self, path):
+        self.path = path
+        if not pathlib.Path(path).is_file():
+            self.reset(path)
+        
+        self.connection = sql.connect(path)
+        self.cursor = self.connection.cursor()
+
+
+    def __del__(self):
+        # save changes before closing
+        self.connection.commit()
+        self.connection.close()
+
+    def reset(self):
+        with open(self.path, 'w') as f:
+            pass
+        
+        connection = sql.connect(self.path)
+        cursor = connection.cursor()
+        now = get_now()
+        cursor.executescript(new_database_script)
+        cursor.execute(update_read_cmd, (now,))
+        cursor.execute(update_write_cmd, (now,))
+        connection.commit()
+        connection.close()
+
+        self.connection = sql.connect(path)
+        self.cursor = self.connection.cursor()
+
+
+    def add_row(self, food:str, date_added, expiration_date):
+        self.cursor.execute(insert_cmd, (food, date_added, expiration_date))
+        self.cursor.execute(update_write_cmd, (get_now(),))
+        self.connection.commit()
+
+    def delete_row(self, food:str):
+        self.cursor.execute(delete_cmd, (food,))
+        self.cursor.execute(update_write_cmd, (get_now(),))
+        self.connection.commit()
     
-    connection = sql.connect(path)
-    cursor = connection.cursor()
-
-    cursor.execute("CREATE TABLE food_data(food, date added, expiration date)")
-    cursor.execute("CREATE TABLE meta_data(key, value)")
-    cursor.execute("INSERT INTO meta_data VALUES('last write time', 20241201)")
-    cursor.execute("INSERT INTO meta_data VALUES('last read time', 20241201)")
-    connection.commit()
-
-def add_row(path, food:str, date_added, expiration_date):
-    connection = sql.connect(path)
-    cursor = connection.cursor()
-
-    cursor.execute(
-        "INSERT INTO food_data VALUES(?, ?, ?)", 
-        (food, date_added, expiration_date)
-    )
-    connection.commit()
-
-def delete_row(path, food:str):
-    connection = sql.connect(path)
-    cursor = connection.cursor()
-
-    cursor.execute(f"DELETE FROM food_data where food='{food}'")
-    connection.commit()
+    def to_list(self):
+        self.cursor.execute(get_all_cmd)
+        return self.cursor.fetchall()
     
-def to_string(path):
-    connection = sql.connect(path)
-    cursor = connection.cursor()
-    # cursor.execute(".headers on")
-
-    cursor.execute("SELECT * from food_data")
-    return cursor.fetchall() 
+    def meta_to_list(self):
+        self.cursor.execute(get_meta_cmd)
+        return self.cursor.fetchall()
 
 if __name__ == "__main__":
     path = "food_data.db"
-    create(path)
-    print(to_string(path))
-    add_row(path, "cheese", 393939, 390393402)
-    print(to_string(path))
-    delete_row(path, "cheese")
-    print(to_string(path))
+    db = SQLiteDB(path)
+    db.reset()
+    print(db.to_list(), db.meta_to_list())
+    db.add_row("cheese", get_now(), get_now())
+    print(db.to_list(), db.meta_to_list())
+    db.delete_row("cheese")
+    print(db.to_list(), db.meta_to_list())
     
